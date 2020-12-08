94c8e675ec78ad0a743851dfdfd119a76e84f546
Leandro Espinosa <tecleandroe@gmail.com>
2020-12-07 22:32:10 -0300

    CP commiteado por Lean-Espinosa a las 1607391130888

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -2,23 +2,37 @@
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb();
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+  let sum = 0;
+  numeros.forEach(function(item) {
+  	sum = sum + item;
+  })
+  cb(sum);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  array.forEach(function(item){
+  	cb(item);
+  })
 }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  let array2 = [];
+  array.forEach(function(item){
+  	array2.push(cb(item));
+  })
+  return array2;
 }
 
 // No modificar nada debajo de esta línea
